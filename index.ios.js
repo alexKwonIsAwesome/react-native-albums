6f2472d9addf23bf9641e40e6a11bc172129a222
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2017-09-25 17:14:26 +0900

    imported Header component to main index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -2,12 +2,13 @@
 
 // Import a library to help create a component
 import React from "react";
-import { AppRegistry, Text } from "react-native";
+import { AppRegistry } from "react-native";
+import Header from "./src/components/header";
 
 // Create a component
 const App = () => {
     return (
-        <Text>Some Text</Text>
+        <Header />
     );
 };
 
