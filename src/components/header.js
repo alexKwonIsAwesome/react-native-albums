603bbf97748a13b0b236d268eff008360a4c370d
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2017-09-25 17:09:39 +0900

    Created Header component

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries fore making a component
+import React from "react";
+import { Text } from "react-native";
+
+const Header = () => {
+    return (
+        <Text>Albums!</Text>
+    );
+};
+
+export default Header;
